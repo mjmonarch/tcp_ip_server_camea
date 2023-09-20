832658d44935db518dda59d13eb279b2237c5ff8
Sergii Kamenskyi <serg@apple.com>
2023-09-20 18:52:59 +0300

    Try another TCPServer lib

---
+++ client.py
@@ -0,0 +1,18 @@
+import socket
+
+
+# for testing
+def client(ip, port, message):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((ip, port))
+        sock.sendall(bytes(message, 'ascii'))
+        # response = str(sock.recv(1024), 'ascii')
+        # print("Received: {}".format(response))
+
+
+if __name__ == "__main__":
+    ip, port = "localhost", 14_000
+
+    client(ip, port, "Hello World 1")
+    client(ip, port, "Hello World 2")
+    client(ip, port, "Hello World 3")
---
+++ simple_tcp.py
@@ -0,0 +1,85 @@
+import threading
+import socketserver
+import logging
+import logging.config
+import os
+import schedule
+import time
+import atexit
+import sys
+
+
+LOG_FILE = "logs/log.log"
+LOGGING_SETTINGS = {
+    "handlers": [],
+    "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    "datefmt": "%d.%m.%Y %H:%M:%S",
+    "level": logging.DEBUG,
+}
+file_handler = logging.handlers.RotatingFileHandler(
+    filename=os.path.join(os.getcwd(), LOG_FILE),
+    encoding="utf-8",
+    mode="a",
+    maxBytes=1_000_000,
+    backupCount=5,
+)
+file_handler.setLevel(logging.INFO)
+LOGGING_SETTINGS["handlers"].append(file_handler)
+
+stream_handler = logging.StreamHandler(stream=sys.stdout)
+LOGGING_SETTINGS["handlers"].append(stream_handler)
+
+logging.basicConfig(**LOGGING_SETTINGS)
+logger = logging.getLogger(__name__)
+
+
+class mTCPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        data = str(self.request.recv(1024), 'ascii')
+        cur_thread = threading.current_thread()
+        response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
+        # self.request.sendall(response)
+        logger.info(response)
+
+
+if __name__ == "__main__":
+    # Port 0 means to select an arbitrary unused port
+    HOST, PORT = "localhost", 14_000
+
+    def __run_scheduler(interval=1):
+        scheduler_event = threading.Event()
+
+        class ScheduleThread(threading.Thread):
+            @classmethod
+            def run(cls):
+                while not scheduler_event.is_set():
+                    schedule.run_pending()
+                    time.sleep(interval)
+
+        continuous_thread = ScheduleThread()
+        continuous_thread.start()
+        return scheduler_event
+
+    def __atexit():
+        stop_scheduler.set()
+
+    def __shutdown(server):
+        logger.info("Server was shutdown because running time expired")
+        server.shutdown()
+
+    # Start the background thread
+    stop_scheduler = __run_scheduler()
+    atexit.register(__atexit)
+
+    server = socketserver.TCPServer((HOST, PORT), mTCPHandler)
+    with server:
+        ip, port = server.server_address
+
+        server_thread = threading.current_thread()
+        logger.info("Server loop running in thread:" + server_thread.name)
+
+        schedule.every(1).minutes.do(__shutdown, server)
+        logger.info("Terminate scheduler set for 1 minutes:" + server_thread.name)
+
+        server.serve_forever()
