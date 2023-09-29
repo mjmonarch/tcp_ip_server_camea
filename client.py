23e01e17d0f074ec5a1135ae5e8b6db06f48640c
Sergii Kamenskyi <serg@apple.com>
2023-09-29 23:30:07 +0300

    add socket server

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ def client(ip, port, message):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((ip, port))
         sock.sendall(bytes(message, 'ascii'))
+        print("a")
         response = str(sock.recv(1024), 'ascii')
         print("Received: {}".format(response))
 
@@ -13,7 +14,5 @@ def client(ip, port, message):
 if __name__ == "__main__":
     ip, port = "localhost", 7_777
 
-    client(ip, port, "Hello World 1")
-    client(ip, port, "Hello World 2")
-    client(ip, port, "Hello World 3")
-    client(ip, port, "")
+    client(ip, port, "Hello World!")
+
---
+++ socket_server.py
@@ -0,0 +1,84 @@
+import threading
+import socket
+import logging
+import logging.config
+import os
+import schedule
+import time
+import atexit
+import sys
+
+
+LOG_FILE = "logs/log.log"
+LOGGING_SETTINGS = {
+    "handlers": [],
+    "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    "datefmt": "%d.%m.%Y %H:%M:%S",
+    "level": logging.DEBUG,
+}
+file_handler = logging.handlers.RotatingFileHandler(
+    filename=os.path.join(os.getcwd(), LOG_FILE),
+    encoding="utf-8",
+    mode="a",
+    maxBytes=1_000_000,
+    backupCount=5,
+)
+file_handler.setLevel(logging.INFO)
+LOGGING_SETTINGS["handlers"].append(file_handler)
+
+stream_handler = logging.StreamHandler(stream=sys.stdout)
+LOGGING_SETTINGS["handlers"].append(stream_handler)
+
+logging.basicConfig(**LOGGING_SETTINGS)
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    # Port 0 means to select an arbitrary unused port
+    HOST, PORT = "127.0.0.1", 7_777
+
+    def __run_scheduler(interval=1):
+        scheduler_event = threading.Event()
+
+        class ScheduleThread(threading.Thread):
+            @classmethod
+            def run(cls):
+                while not scheduler_event.is_set():
+                    schedule.run_pending()
+                    time.sleep(interval)
+
+        continuous_thread = ScheduleThread()
+        continuous_thread.start()
+        return scheduler_event
+
+    def __atexit():
+        stop_scheduler.set()
+
+    def __shutdown(s):
+        logger.info("Server was shutdown because running time expired")
+        s.close()
+        stop_scheduler.set()
+
+    # Start the background thread
+    stop_scheduler = __run_scheduler()
+    atexit.register(__atexit)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+
+        socket_thread = threading.current_thread()
+        logger.info("Start socket listening in thread:" + socket_thread.name)
+
+        schedule.every(1).minutes.do(__shutdown, s)
+        logger.info("Terminate scheduler set for 1 minutes:" + socket_thread.name)
+
+        try:
+            conn, addr = s.accept()
+            with conn:
+                logger.info("Connection established with: " + str(addr))
+                while True:
+                    data = conn.recv(1024)
+                    logger.info(f"Received data: '{data}' from {str(addr)}")
+                    conn.sendall(bytearray(b'\x48\x53\x78\x78'))
+        except KeyboardInterrupt:
+            __atexit()
