58958d79d8e2cf4dbbe0de4f4cfc2b1845f78f60
Sergii Kamenskyi <serg@apple.com>
2023-11-18 17:44:15 +0200

    adding vidar_service

--- vidar_service.py
+++ vidar_service.py
@@ -47,6 +47,7 @@ class VidarService:
         t1 = int(transit_timestamp.timestamp()*1_000) - tolerance
         t2 = int(transit_timestamp.timestamp()*1_000) + tolerance
         url = 'https://' + self.IP + f'/lpr/cff?cmd=querydb&sql=select%20*%20from%20cffresult%20where%20frametimems%20%3E%{t1}%20and%20frametimems%20%3C%{t2}'
+        print(url)
         r = requests.get(url)
         root = ET.fromstring(r.content)
         for row in root.findall('row'):
