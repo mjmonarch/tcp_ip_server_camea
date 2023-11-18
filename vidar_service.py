06507eabb4f6ada78d9ef1ae94aacaf30413fec8
Sergii Kamenskyi <serg@apple.com>
2023-11-18 20:48:22 +0200

    adding vidar_service

--- vidar_service.py
+++ vidar_service.py
@@ -73,7 +73,7 @@ class VidarService:
         print(url)
         r = requests.get(url)
         root = ET.fromstring(r.content)
-        print(root.tag)
+        print(root.findall('ID'))
         if root.find('ID'):
             result['timestamp'] = root.find('frametimems').get('value')
             print(result)
