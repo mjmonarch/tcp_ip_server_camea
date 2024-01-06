500f0d97ca33de2b7b6cfb858fb18ae0b6c7743d
Sergii Kamenskyi <serg@apple.com>
2024-01-06 19:48:30 +0200

    Major refectoring: adding reconnection to camea

--- camea_service.py
+++ camea_service.py
@@ -92,7 +92,10 @@ class CameaService:
         schedule.clear()
         self.stop_scheduler.set()
         self.conn.close()
+        ### DDD
+        logger.info("0000000000000000")
         sys.exit(1)
+        logger.info("1111111111111111")
 
     def __create_connection(self):
         conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
