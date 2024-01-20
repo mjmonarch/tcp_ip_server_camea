8085811a552e824981ddb259d0df27aec7e884e7
Sergii Kamenskyi <serg@apple.com>
2024-01-20 16:31:59 +0200

    return postpone processing

--- socket_server.py
+++ socket_server.py
@@ -361,8 +361,12 @@ class QUERY_PROCESSOR:
                                     logger.info(f"Received data: {query} from "
                                                 + str(self.camea_client_address))
                                     logger.debug("DetectionRequest catched")
-                                    self.process_DetectionRequest(data=query,
-                                                                  conn=self.camea_client)
+                                    # send software trigger to vidar
+                                    # self.vidar_service.send_software_trigger()
+                                    # postpone detection request processing
+                                    schedule.every(3).seconds.do(self.process_DetectionRequest(
+                                                                 data=query,
+                                                                 conn=self.camea_client)).tag("detection")
                                 else:
                                     logger.debug('not a DetectionRequest')
                             except IncorrectCameaQuery as e:
@@ -373,13 +377,16 @@ class QUERY_PROCESSOR:
                     logger.error('Connection with Camea Management system was closed by Camea: '
                                  + str(e))
                     schedule.cancel_job(keep_alive_job)
+                    schedule.clear("detection")
                 except TimeoutError:
                     logger.error('Connection to Camea Management system was closed due to timeout')
                     schedule.cancel_job(keep_alive_job)
+                    schedule.clear("detection")
                 except SocketCorrupted as e:
                     logger.error('Connection with Camea Management system was corrupted: '
                                  + str(e))
                     schedule.cancel_job(keep_alive_job)
+                    schedule.clear("detection")
                     continue
             except KeyboardInterrupt:
                 __stop_server(socket_server, 'keyboard interrupt')
@@ -387,6 +394,7 @@ class QUERY_PROCESSOR:
                 logger.error('An error occured during runtime: ' + str(e))
                 logger.info(f'camea client: {self.camea_client}')
                 schedule.cancel_job(keep_alive_job)
+                schedule.clear("detection")
                 continue
 
 
