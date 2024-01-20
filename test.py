605870e473e8b5aa5c22eab0f54efc5c5403c1a3
Sergii Kamenskyi <serg@apple.com>
2024-01-20 16:01:04 +0200

    add simple listener to check camea push

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import schedule
 import time
 import atexit
 import sys
+from errors import SocketCorrupted
 
 
 LOG_FILE = "logs/log.log"
@@ -34,20 +35,6 @@ logging.basicConfig(**LOGGING_SETTINGS)
 logger = logging.getLogger(__name__)
 
 
-class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
-
-    def handle(self):
-        data = str(self.request.recv(1024), 'ascii')
-        cur_thread = threading.current_thread()
-        response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
-        # self.request.sendall(response)
-        logger.info(response)
-
-
-class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    pass
-
-
 # for testing
 def client(ip, port, message):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -58,50 +45,24 @@ def client(ip, port, message):
 
 
 if __name__ == "__main__":
-    # Port 0 means to select an arbitrary unused port
-    HOST, PORT = "localhost", 14_000
+    HOST, PORT = "localhost", 50501
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    logger.info(f'Connecting to Camea XXX at {HOST}: {PORT}')
+    conn.connect((HOST, PORT))
+    # handshake
+    conn.sendall(bytearray(b'\x4b\x41\x78\x78\x00\x00\x00\x00\x00\x00\x00\x00'))
+    logger.info(f"Handshake was sent to {conn.getpeername()}")
+    s2_response = str(conn.recv(), 'ascii')
+    logger.info((f"Received data: '{s2_response}'"
+                 + f"from {HOST}:{PORT}"))
 
-    # def __run_scheduler(interval=1):
-    #     scheduler_event = threading.Event()
-
-    #     class ScheduleThread(threading.Thread):
-    #         @classmethod
-    #         def run(cls):
-    #             while not scheduler_event.is_set():
-    #                 schedule.run_pending()
-    #                 time.sleep(interval)
-
-    #     continuous_thread = ScheduleThread()
-    #     continuous_thread.start()
-    #     return scheduler_event
-
-    # def __atexit():
-    #     stop_scheduler.set()
-
-    # def __shutdown(server):
-    #     logger.info("Server was shutdown because running time expired")
-    #     server.shutdown()
-
-    # Start the background thread
-    # stop_scheduler = __run_scheduler()
-    # atexit.register(__atexit)
-
-    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
-    with server:
-        ip, port = server.server_address
-
-        # Start a thread with the server -- that thread will then start one
-        # more thread for each request
-        server_thread = threading.Thread(target=server.serve_forever)
-        # Exit the server thread when the main thread terminates
-        server_thread.daemon = True
-        server_thread.start()
-        # logger.info("Server loop running in thread:" + server_thread.name)
-
-        # schedule.every(10).minutes.do(__shutdown, server)
-        # logger.info("Terminate scheduler set for 10 minutes:" + server_thread.name)
-
-        # client(ip, port, "Hello World 1")
-        # client(ip, port, "Hello World 2")
-        # client(ip, port, "Hello World 3")
-
+    while conn:
+        try:
+            data = conn.recv()
+        except AttributeError:
+            raise SocketCorrupted("can't read from socket")    
+        try:
+            data = data.decode('ISO-8859-1')
+        except Exception as e:
+            logger.error(f"Failed to decode: '{data}' - " + str(e))
+        logger.info(f"Data received: {data}")
