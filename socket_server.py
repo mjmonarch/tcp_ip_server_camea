5f0ea9bb64caca122a2d64aa530e7ac252d0e88f
Sergii Kamenskyi <serg@apple.com>
2024-01-20 16:53:18 +0200

    return postpone processing

--- socket_server.py
+++ socket_server.py
@@ -22,7 +22,7 @@ LOG_FILE = "logs/log.log"
 LOGGING_SETTINGS = {
     "handlers": [],
     "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    "datefmt": "%d.%m.%Y %H:%M:%S",
+    "datefmt": "%d.%m.%Y %H:%M:%S.%03d",
     "level": logging.DEBUG,
 }
 file_handler = logging.handlers.RotatingFileHandler(
